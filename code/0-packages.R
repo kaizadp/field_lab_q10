9a0e3345f9d2e43bb8b8ad2bb7b572c1d609ca73
kaizadp <50244730+kaizadp@users.noreply.github.com>
2021-03-31 11:03:57 -0700

    add maps packages

--- code/0-packages.R
+++ code/0-packages.R
@@ -1,4 +1,8 @@
 library(tidyverse)
 library(drake)
+library(nlme) # for LME
+library(rnaturalearth) # for world maps
+library(rnaturalearthdata) # for world maps
+library(sf) # for world maps
 
-theme_set(theme_bw())
+theme_set(theme_classic())
