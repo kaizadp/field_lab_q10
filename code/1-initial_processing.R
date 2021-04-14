65284699b0b118d3d90768c8f925c94ba58beb94
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2021-04-14 10:10:33 -0400

    Follow R assignment conventions

--- code/1-initial_processing.R
+++ code/1-initial_processing.R
@@ -2,30 +2,30 @@ library(tidyverse)
 theme_set(theme_bw())
 
 
-clean_srdb_dataset = function(){
-  srdb_v5_data = read.csv("data/SRDB_V5_1827/data/srdb-data-V5.csv")
-  srdb_v5_equations = read.csv("data/SRDB_V5_1827/data/srdb-equations-V5.csv")
+clean_srdb_dataset <- function(){
+  srdb_v5_data <- read.csv("data/SRDB_V5_1827/data/srdb-data-V5.csv")
+  srdb_v5_equations <- read.csv("data/SRDB_V5_1827/data/srdb-equations-V5.csv")
   
-  sites =
+  sites <-
     srdb_v5_data %>% 
     dplyr::select(Record_number, Study_number, Entry_date, Duplicate_record, 
                   Latitude, Longitude, Elevation, Biome, Ecosystem_type, 
                   Manipulation, Manipulation_level, Meas_method) %>% 
     filter(Manipulation == "None"|Manipulation_level %in% c("None", "NONE", "none"))
   
-  r10 = 
+  r10 <-
     srdb_v5_equations %>% 
     dplyr::select(Record_number, R10)
   
   
   process_q10_data = function(srdb_v5_equations){
-    q10_1 = 
+    q10_1 <-
       srdb_v5_equations %>% 
       dplyr::select(Record_number, Q10_0_10, Q10_5_15, Q10_10_20, Q10_0_20) %>% 
       pivot_longer(-Record_number, names_to = "temp_range", values_to = "Q10") %>% 
       mutate(temp_range = str_remove(temp_range, "Q10_")) 
     
-    q10_other1 = 
+    q10_other1 <- 
       srdb_v5_equations %>% 
       dplyr::select(Record_number, starts_with("Q10_other1")) %>% 
       drop_na() %>% 
@@ -37,7 +37,7 @@ clean_srdb_dataset = function(){
       mutate(Q10 = round(Q10, 2)) %>% 
       dplyr::select(Record_number, temp_range, Q10)
     
-    q10_other2 = 
+    q10_other2 <- 
       srdb_v5_equations %>% 
       dplyr::select(Record_number, starts_with("Q10_other2")) %>% 
       drop_na() %>% 
@@ -49,21 +49,21 @@ clean_srdb_dataset = function(){
     bind_rows(q10_1, q10_other1, q10_other2) %>% filter(Q10 <= 500)
     
   }
-  q10 = process_q10_data(srdb_v5_equations)
+  q10 <- process_q10_data(srdb_v5_equations)
   
-  r10_sites = sites %>% left_join(r10) %>% filter(!is.na(R10))
-  q10_sites = sites %>% left_join(q10) %>% filter(!is.na(Q10))
+  r10_sites <- sites %>% left_join(r10) %>% filter(!is.na(R10))
+  q10_sites <- sites %>% left_join(q10) %>% filter(!is.na(Q10))
 
   list(r10_sites = r10_sites,
        q10_sites = q10_sites)
   
 }
 
-do_q10_exploration = function(){
+do_q10_exploration <- function(){
   srdb_v5_equations = read.csv("data/SRDB_V5_1827/data/srdb-equations-V5.csv")
   
   
-  temp_effect = 
+  temp_effect <-
     srdb_v5_equations %>% 
     dplyr::select(Temp_effect, Q10_5_15, Q10_0_20, Q10_0_10) %>% 
     pivot_longer(-Temp_effect)
@@ -74,7 +74,7 @@ do_q10_exploration = function(){
   
 }
 
-import_individual_studies = function(){
+import_individual_studies <- function(){
   
   filePaths_field <- list.files(path = "data/cleaned_for_analysis/field",pattern = "*.csv", full.names = TRUE)
   filePaths_lab <- list.files(path = "data/cleaned_for_analysis/lab",pattern = "*.csv", full.names = TRUE)
@@ -90,7 +90,7 @@ import_individual_studies = function(){
     df})) %>% 
     mutate(Incubation = "lab")
   
-  combined = 
+  combined <- 
     bind_rows(lab_data, field_data) %>% 
     dplyr::select(Source, Incubation, Record_number, Duplicate_record,
                   Site_name, Latitude_deg, Longitude_deg, Latitude, Longitude,
@@ -98,9 +98,9 @@ import_individual_studies = function(){
                   Temp_range, Temp_mean, Temp_min, Temp_max, Q10, R10) %>% 
     na_if("")
   
-fix_temp_and_latlon = function(combined){
+fix_temp_and_latlon <- function(combined){
   # clean temperature ranges ----
-  combined2 = 
+  combined2 <- 
     combined %>% 
     mutate(Temp_min = round(Temp_min/5)*5,
            Temp_max = round(Temp_max/5)*5,
@@ -114,7 +114,7 @@ fix_temp_and_latlon = function(combined){
   
   # fix latitude/longitude ----
   # subset only the columns needed, we will re-join later
-  combined2_latlong = 
+  combined2_latlong <- 
     combined2 %>% 
     dplyr::select(rownum, Latitude, Longitude, Latitude_deg, Longitude_deg) %>% 
     # create a separate column for hemisphere
@@ -193,9 +193,9 @@ fix_temp_and_latlon = function(combined){
   fix_temp_and_latlon(combined)
 }
 
-combine_all_q10_studies = function(indiv_studies, srdb_q10){
+combine_all_q10_studies <- function(indiv_studies, srdb_q10){
   # combine the available data ----
-  q10_combined = 
+  q10_combined <- 
     indiv_studies %>% 
     dplyr::select(Source, Incubation, Latitude, Longitude, Temp_range, Temp_mean, Q10) %>% 
     bind_rows(srdb_q10 %>% 
@@ -203,7 +203,7 @@ combine_all_q10_studies = function(indiv_studies, srdb_q10){
                 rename(Temp_range = temp_range))
   
   # clean the temperature ranges ----
-  temp_ranges = 
+  temp_ranges <- 
     q10_combined %>% 
     dplyr::select(Temp_range) %>% 
     distinct() %>% 
@@ -221,7 +221,7 @@ combine_all_q10_studies = function(indiv_studies, srdb_q10){
                        Temp_min >= 25 ~ "> 25"))
   
   # 
-  q10_combined_temp = 
+  q10_combined_temp <- 
     q10_combined %>% 
     left_join(temp_ranges %>% dplyr::select(Temp_range, Temp_range_new)) %>% 
     mutate(Temp_range_new = if_else(is.na(Temp_range),
@@ -238,12 +238,12 @@ combine_all_q10_studies = function(indiv_studies, srdb_q10){
   q10_combined_temp
 }
 
-make_map_all_studies = function(combined_q10){
+make_map_all_studies <- function(combined_q10){
   library(rnaturalearth)
   library(rnaturalearthdata)
   library(sf)
   
-  world = ne_countries(scale = "medium",  returnclass = "sf", type = "countries")
+  world <- ne_countries(scale = "medium",  returnclass = "sf", type = "countries")
   
   world %>% 
     ggplot()+
@@ -256,14 +256,14 @@ make_map_all_studies = function(combined_q10){
     theme(legend.position = "top")
 }
 
-compute_stats_q10 = function(combined_q10){
+compute_stats_q10 <- function(combined_q10){
   # LME ----
-  l = nlme::lme(Q10 ~ Incubation, random = ~1|Incubation, data = combined_q10) 
+  l <- nlme::lme(Q10 ~ Incubation, random = ~1|Incubation, data = combined_q10) 
   anova(l)
   # NaNs produced
   
   # ANOVA/HSD ----
-  fit_aov = function(combined_q10){
+  fit_aov <- function(combined_q10){
     a = aov(Q10 ~ Incubation, data = combined_q10)
     # h = agricolae::HSD.test(a, "Incubation")
     # h$groups %>% 
@@ -284,9 +284,9 @@ compute_stats_q10 = function(combined_q10){
   
   }
 
-make_graphs_q10 = function(combined_q10){
+make_graphs_q10 <- function(combined_q10){
   
-  resp_q10_temp = 
+  resp_q10_temp <- 
     combined_q10 %>% 
     ggplot(aes(x = Temp_range_new, y = Q10, color = Incubation))+
     geom_point(position = position_dodge(width = 0.4))
@@ -303,16 +303,16 @@ make_graphs_q10 = function(combined_q10){
 
 #
 ## misc code ----
-more_data = import_individual_studies()
+more_data <- import_individual_studies()
 
-more_data_srdb = more_data %>% 
+more_data_srdb <- more_data %>% 
   bind_rows(srdb_q10 %>% 
               mutate(Record_number = as.character(Record_number),
                                 Incubation = "field",
                                 Source = "SRDB") %>% 
               rename(Temp_range = temp_range))
 
-more_data2 = 
+more_data2 <- 
   more_data_srdb %>% 
   dplyr::select(Source, Incubation, Q10, Temp_range, Temp_mean, Latitude, Longitude) %>% 
   mutate(Temp_range2 = Temp_range) %>% 
@@ -332,9 +332,9 @@ more_data2 %>%
   ggplot(aes(y = Latitude, x = Q10, color = Incubation))+
   geom_point()
 
-hamdi = read_csv("data/cleaned_for_analysis/lab/Hamdi2013.csv")
+hamdi <- read_csv("data/cleaned_for_analysis/lab/Hamdi2013.csv")
 
-hamdi2 = 
+hamdi2 <- 
   hamdi %>% 
   dplyr::select(latitude_deg) %>% 
   mutate(latitude_deg = if_else(grepl('"', latitude_deg), 
@@ -367,12 +367,12 @@ hamdi2 =
   # use the `sidb` package to load the data and then
   # flatten the large list into a list with two nested lists 
   load("data/sidb.RData")
-  sidb_flat = flatterSIDb(sidb)
+  sidb_flat <- flatterSIDb(sidb)
   
   # convert the two lists into dataframes
   # vars needs data.table::rbindlist because it is a different format 
-  sidb_timeseries = dplyr::bind_rows(sidb_flat$timeseries)
-  sidb_vars = rbindlist(sidb_flat$vars, fill = TRUE)
+  sidb_timeseries <- dplyr::bind_rows(sidb_flat$timeseries)
+  sidb_vars <- rbindlist(sidb_flat$vars, fill = TRUE)
   
   ## next steps:
   # - combine data with metadata, using ID - done
@@ -381,28 +381,28 @@ hamdi2 =
   # - exclude glucose additions - done
   # - exclude data with only a single temperature level
   
-  clean_sidb_data = function(sidb_vars, sidb_timeseries){
-    sidb_vars_clean = 
+  clean_sidb_data <- function(sidb_vars, sidb_timeseries){
+    sidb_vars_clean <- 
       sidb_vars %>% 
       filter(!units %in% c("permille", "percentC14Remaining")) %>% 
       filter(is.na(elevatedCO2) | elevatedCO2 == "control") %>% 
       filter(is.na(glucose)) %>% 
       filter(is.na(cellulose) | cellulose == "control")
     
-    sidb_timeseries_clean = 
+    sidb_timeseries_clean <- 
       sidb_timeseries %>% 
       filter(time <= 370) %>% 
       left_join(sidb_vars_clean %>% dplyr::select(ID, temperature, units, citationKey)) %>% 
       drop_na() 
     
       # keep only data with multiple temperature levels
-      temp_count = 
+      temp_count <-
         sidb_timeseries_clean %>% 
         distinct(citationKey, temperature) %>%
         group_by(citationKey) %>% 
         dplyr::mutate(n = n())
       
-      sidb_timeseries_clean2 = 
+      sidb_timeseries_clean2 <- 
         sidb_timeseries_clean %>% 
         left_join(temp_count) %>% 
         filter(n > 1) %>% 
@@ -412,22 +412,22 @@ hamdi2 =
     list(sidb_vars_clean = sidb_vars_clean,
          sidb_timeseries_clean2 = sidb_timeseries_clean2)
   }
-  sidb_vars_clean = clean_sidb_data(sidb_vars, sidb_timeseries)$sidb_vars_clean
-  sidb_timeseries_clean = clean_sidb_data(sidb_vars, sidb_timeseries)$sidb_timeseries_clean2
+  sidb_vars_clean <- clean_sidb_data(sidb_vars, sidb_timeseries)$sidb_vars_clean
+  sidb_timeseries_clean <- clean_sidb_data(sidb_vars, sidb_timeseries)$sidb_timeseries_clean2
   
-  calculate_sidb_q10_r10 = function(sidb_timeseries_clean){
+  calculate_sidb_q10_r10 <- function(sidb_timeseries_clean){
     # using equations from Meyer et al. 2018. https://doi.org/10.1002/2017GB005644
-    fit_q10_parameters = function(sidb_timeseries_clean){
-      curve.nls = nls(response ~ a*exp(temperature * b),
+    fit_q10_parameters <- function(sidb_timeseries_clean){
+      curve.nls <- nls(response ~ a*exp(temperature * b),
                         start=list(a=5,
                                    b=2),
                         data = sidb_timeseries_clean)
-      coef1 = coef(curve.nls) %>% as.data.frame() 
-      coef_transpose = coef1 %>% transpose() %>% `colnames<-`(rownames(coef1))
+      coef1 <- coef(curve.nls) %>% as.data.frame() 
+      coef_transpose <- coef1 %>% transpose() %>% `colnames<-`(rownames(coef1))
       coef_transpose
     }
     
-    a =   
+    a <-   
       sidb_timeseries_clean %>% 
       group_by(citationKey, time) %>% 
       do(fit_q10_parameters(.))
@@ -438,15 +438,3 @@ hamdi2 =
   
 
 # -------------------------------------------------------------------------
-
-  
-  
-  
- 
-
-
-
-
-
-
-
