10952e5ebbddf62dbe915e74f74270db611fbe93
kaizadp <50244730+kaizadp@users.noreply.github.com>
2021-04-15 14:31:51 -0700

    update packages

--- code/0-packages.R
+++ code/0-packages.R
@@ -4,5 +4,7 @@ library(nlme) # for LME
 library(rnaturalearth) # for world maps
 library(rnaturalearthdata) # for world maps
 library(sf) # for world maps
+library(sidb) # Soil Incubation Database 
+library(data.table)
 
 theme_set(theme_classic())
