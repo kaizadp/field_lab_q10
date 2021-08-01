b18108015160ba82a6c169ac8ad55e72806f7b8b
kaizadp <50244730+kaizadp@users.noreply.github.com>
2021-08-01 14:56:25 -0700

    create drake plan for data analysis

---
+++ code/5-plan_analysis.R
@@ -0,0 +1,22 @@
+source("code/0-packages.R")
+source("code/3-analysis_functions.R")
+
+
+analysis_plan = drake_plan(
+  Q10_data = read.csv("data/processed/Q10_data.csv", na.strings = "") %>% 
+    reorder_temp_levels(.),
+  
+  Q10_map = make_map_all_studies(Q10_data),
+  Q10_stats = compute_stats_q10(Q10_data),
+  Q10_graphs = make_graphs_q10(Q10_data),
+  
+  study_summary = compute_study_summary(Q10_data),
+  
+  # Reports ----
+  report = rmarkdown::render(
+    knitr_in("reports/2-data_analysis.Rmd"),
+    output_format = rmarkdown::github_document()), quiet = T,
+)
+
+
+make(analysis_plan)
