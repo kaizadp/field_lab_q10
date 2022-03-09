d2cf6d2edeaaa1109f59ff7d11ee617bdd790771
kaizadp <50244730+kaizadp@users.noreply.github.com>
2022-03-08 19:35:50 -0800

    comment out q10_fitting code

--- code/5-plan-analysis.R
+++ code/5-plan-analysis.R
@@ -1,14 +1,12 @@
 source("code/0-packages.R")
 source("code/3-functions-analysis.R")
-source("code/4-functions-q10_fitting.R")
+# source("code/4-functions-q10_fitting.R")
 
 
 analysis_plan = drake_plan(
   #Q10 from papers ----
   Q10_data = read.csv("data/processed/Q10_data.csv", na.strings = "") %>% 
-    reorder_temp_levels(.) %>% 
-    mutate(Ecosystem_type = tolower(Ecosystem_type),
-           Biome = tolower(Biome)),
+    reorder_temp_levels(.),
   
   Q10_map = make_map_all_studies(Q10_data),
   Q10_stats = compute_stats_q10(Q10_data),
@@ -20,8 +18,8 @@ analysis_plan = drake_plan(
   #
   # fit Q10 ----
   
-  q10_calculated = compute_q10(sidb_timeseries_clean),
-  gg_q10_calculated = plot_q10_functions(q10_calculated),
+ # q10_calculated = compute_q10(sidb_timeseries_clean),
+ #  gg_q10_calculated = plot_q10_functions(q10_calculated),
   
   #
   # Reports ----
