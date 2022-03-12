f1a798352fa4e3f8a86da9141ea57c431214c153
kaizadp <50244730+kaizadp@users.noreply.github.com>
2022-03-11 20:29:52 -0800

    set palettes. update plan.

--- code/5-plan-analysis.R
+++ code/5-plan-analysis.R
@@ -2,18 +2,43 @@ source("code/0-packages.R")
 source("code/3-functions-analysis.R")
 # source("code/4-functions-q10_fitting.R")
 
+# set palettes
+
+pal_biome = PNWColors::pnw_palette("Sailboat", 5)
+pal_incubation = soilpalettes::soil_palette("redox2", 3)
+
 
 analysis_plan = drake_plan(
-  #Q10 from papers ----
-  Q10_data = read.csv("data/processed/Q10_data.csv", na.strings = "") %>% 
-    reorder_temp_levels(.),
+  # import processed Q10 data ----
+  Q10 = read.csv("data/processed/Q10_data.csv", na.strings = ""),
+  sample_metadata = read.csv("data/processed/Q10_sample_metadata.csv", na.strings = ""),
+  Q10_data = left_join(Q10, 
+                       sample_metadata %>% dplyr::select(
+                         Q10_record_number, Latitude, Longitude, MAT, MAP, ClimateTypes)) %>% 
+    reorder_species_levels(.) %>% reorder_temp_levels(.) %>% reorder_biome_levels(.),
+  # exploration ----
   
   Q10_map = make_map_all_studies(Q10_data),
-  Q10_stats = compute_stats_q10(Q10_data),
-  Q10_graphs = make_graphs_q10(Q10_data),
-  Q10_rh_only = compute_rh_only(Q10_data),
-  
   study_summary = compute_study_summary(Q10_data),
+  gg_temp_ranges = plot_temperature_ranges(Q10_data),
+  gg_mat_map = plot_mat_map(Q10_data),
+  
+  
+  # analysis - CO2 ----
+  co2_all = compute_co2_all(Q10_data),
+  co2_temp_range = compute_co2_temp_range(Q10_data),
+  co2_biome = compute_co2_biome(Q10_data),
+  
+  co2_bootstrapping = compute_co2_bootstrapping(Q10_data),
+  Q10_rh_only = compute_rh_only(Q10_data),
+
+  
+  # analysis - CH4 ----
+  ch4_all = compute_ch4(Q10_data),
+    
+  #Q10_stats = compute_stats_q10(Q10_data),
+  #Q10_graphs = make_graphs_q10(Q10_data),
+  
   
   #
   # fit Q10 ----
