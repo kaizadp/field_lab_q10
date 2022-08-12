bb49ea31f3775eddcb9a53bbd344dabaa9e9fae8
kaizadp <50244730+kaizadp@users.noreply.github.com>
2022-08-12 09:54:16 -0700

    SIDB install code

--- code/0-packages.R
+++ code/0-packages.R
@@ -4,6 +4,7 @@ library(nlme) # for LME
 library(rnaturalearth) # for world maps
 library(rnaturalearthdata) # for world maps
 library(sf) # for world maps
+#devtools::install_github('SoilBGC-Datashare/sidb/Rpkg')
 library(sidb) # Soil Incubation Database 
 library(data.table)
 
