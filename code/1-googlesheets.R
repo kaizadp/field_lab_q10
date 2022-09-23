2c6d793799b89380c46d5fafb75337951d3228df
Kaizad Patel <50244730+kaizadp@users.noreply.github.com>
2022-09-22 20:38:37 -0700

    remove N

--- code/1-googlesheets.R
+++ code/1-googlesheets.R
@@ -12,15 +12,6 @@ library(googlesheets4)
 
 
 # download from Google Drive ----------------------------------------------
-load_N_files_from_googlesheets = function(gsheets_path_N){
-  n_data = 
-    googlesheets4::read_sheet(gsheets_path_N) 
-  
-  n_data %>% 
-    mutate_all(as.character) %>% 
-    mutate(across(.fns = ~replace(., . == "NULL", NA))) %>% 
-    mutate(Q10 = as.numeric(Q10))
-}
 
 load_CH4_files_from_googlesheets = function(gsheets_path_CH4){
   ch4_data = 
@@ -45,15 +36,12 @@ load_CO2_files_from_googlesheets = function(gsheets_path_CH4){
 }
 
 
-gsheets_path_N = "1uBhsn7s6LJTl0CLYl7IPGQ6dM0tPnFiMY0iShcmIUX4"
 gsheets_path_CH4 = "1GIq2R8afh2p8tULEDuPmS2y-hSUTp8_wXTde2Wz5q8k"
 gsheets_path_CO2 = "1hgpGLFuVAhnV7mfCThyXu2ApFLeC7sIR91cQ_I9TP7c"
 
 
-gsheets_data_N = load_N_files_from_googlesheets(gsheets_path_N)
 gsheets_data_CH4 = load_CH4_files_from_googlesheets(gsheets_path_CH4)
 gsheets_data_CO2 = load_CO2_files_from_googlesheets(gsheets_path_CO2)
 
-gsheets_data_N %>% write.csv(GSHEETS_LOCAL_PATH_N, row.names = FALSE, na = "")
 gsheets_data_CH4 %>% write.csv(GSHEETS_LOCAL_PATH_CH4, row.names = FALSE, na = "")
 gsheets_data_CO2 %>% write.csv(GSHEETS_LOCAL_PATH_CO2, row.names = FALSE, na = "")
